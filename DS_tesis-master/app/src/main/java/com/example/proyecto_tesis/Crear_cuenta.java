a9b39c6a7ac9f2e5c53e1c1f74f5ab0a56b6bd39
CisneFeijoo1998 <mdfeijoo@espol.edu.ec>
2020-11-21 16:40:15 -0500

    hola soy Cisne

---
+++ DS_tesis-master/app/src/main/java/com/example/proyecto_tesis/Crear_cuenta.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_tesis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Crear_cuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+    }
+}
